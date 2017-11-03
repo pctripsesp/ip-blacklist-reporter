9c5d7ad6b35d4242f94e4c764b620d44e4b4ffb8
pctripsesp <pctripsesp@users.noreply.github.com>
2017-11-03 21:00:31 +0100

    whitelist added

--- blackListMaker.py
+++ blackListMaker.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# THIS SCRIPT PRINTS IP ADDRESSES FROM SERVER'S LOG FILE LIKE: 
+# THIS SCRIPT PRINTS IP ADDRESSES FROM SERVER'S LOG FILE LIKE:
 '''
 Warning,Sistema,2017/11/01 17:22:33,SYSTEM,Host [218.67.107.3] was blocked via [SSH].
-
 Warning,Sistema,2017/11/01 17:19:31,SYSTEM,Host [59.27.35.119] was blocked via [SSH].
-
 Warning,Sistema,2017/11/01 17:11:42,SYSTEM,Host [92.111.181.206] was blocked via [SSH].
 '''
 
@@ -13,9 +11,10 @@ Warning,Sistema,2017/11/01 17:11:42,SYSTEM,Host [92.111.181.206] was blocked via
 #LOG FILE PATH
 filename = "syslog_2017-11-1-17_30_32.csv"
 blackList = []
+whiteList = []  #Put your IP white list here in order to not be reported to blackList servers
 
 def makeBL():
-    
+
     with open(filename) as f:
         while True:
             line = f.readline()
@@ -23,13 +22,14 @@ def makeBL():
                 start = line.find('[') + 1
                 end = line.find(']', start)
                 ip = line[start:end]
-                blackList.append(ip)               
+                if ip not in whiteList:
+                    blackList.append(ip)
 
             if not line:
                 break
 
     return blackList
-	
+
 
 if __name__ == "__main__":
     cont=0
